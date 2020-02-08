f407389408a3202c2759f8c465b47dfd65d863e4
Jo Walsh <369631+metazool@users.noreply.github.com>
2020-02-08 08:05:28 +0000

    Overwrite option so we can re-run (kernel timed out)

--- forams_download.py
+++ forams_download.py
@@ -10,12 +10,12 @@ START = 'http://endlessforams.org/summary'
 DATA = 'http://endlessforams.org/randomizer/download/{0}/{1}?download=capsule.zip'
 
 
-def download_capsules():
+def download_capsules(overwrite=False):
     summary = requests.get(START).json()
     for taxon in summary['results']:
         download_data(taxon['sci_name'], taxon['amount_images'])
 
-def download_data(name, number):
+def download_data(name, number, overwrite=False):
     url = DATA.format(name, number)
     # "results": [{"sci_name": "Beella digitata", "amount_images": 40},
     dir_name = os.path.join(os.getcwd(), 'data', name.replace(' ','_'))
@@ -24,6 +24,8 @@ def download_data(name, number):
     except OSError as e:
         if e.errno != errno.EEXIST:
             raise
+        elif not overwrite:
+            return
             
     r = requests.get(url, stream=True)
     try:
